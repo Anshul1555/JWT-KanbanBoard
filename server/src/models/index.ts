68d1d952c9e50f077f5983f313e5d5a9709dac6c
Anshul1555 <104417133+Anshul1555@users.noreply.github.com>
2025-03-26 13:54:05 -0400

    Update index.ts

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -18,7 +18,7 @@ const sequelize = process.env.DB_URL
     })
   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
       host: process.env.DB_HOST || 'localhost',
-      port: process.env.DB_PORT || 5432,
+      port: Number(process.env.DB_PORT) || 5432,
       dialect: 'postgres',
       logging: false,
       dialectOptions: {
