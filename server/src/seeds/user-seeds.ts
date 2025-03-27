e03f4bbd6c1c6c11ce15d25da9801d45a2d985bf
Anshul Sharma <AnshulSharma9366@gmail.com>
2025-03-27 11:39:09 -0400

    updated seed

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -5,7 +5,7 @@ export const seedUsers = async () => {
     console.log('Seeding users...');
 
     const users = await User.bulkCreate([
-      { username: 'JollyGuru', password: 'password' },
+      { username: 'Anshul', password: 'password' },
       { username: 'SunnyScribe', password: 'password' },
       { username: 'RadiantComet', password: 'password' },
     ], { individualHooks: true });
